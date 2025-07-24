a0bf3cb53567a18b62afc0fc6d3d664bb3f8f852
filipeped <filipepedoo@gmail.com>
2025-07-24 11:45:44 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home() {
     const payload = {
       data: [event],
       pixel_id: "765087775987515",
-      access_token: "EAAQfmxkTTZCcBPHGbA2ojC29bVbNPa6GM3nxMxsZC29ijBmuyexVifaGnrjFZBZBS6LEkaR29X3tc5TWn4SHHffeXiPvexZAYKP5mTMoYGx5AoVYaluaqBTtiKIjWALxuMZAPVcBk1PuYCb0nJfhpzAezh018LU3cT45vuEflMicoQEHHk3H5YKNVAPaUZC6yzhcQZDZD"
+      access_token: "EAAQfmxkTTZCcBPEcIiZAy6OKIxlt0ZBQxYHCcVT3pLFZAOR6zr1PjKMOV844YNtdNFp2PRgwkBvhFIYLv5ZBVnltHDRnlZAAZCH8gIZCK0HHs6dR4QpWOUKxKvoaUXvUobAsJZB4c0eraiIzoJFkPeanQ6v7PPzzDqIZB6dniUDyQK6PhwrVPGLmzgPoaDzz9bYwZDZD"
     };
 
     try {
